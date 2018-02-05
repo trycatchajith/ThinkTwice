d06147b4c909095b4b1dd4738bc3c0eaa957a32b
trycatchajith <ajithurulikunnam@gmail.com>
2018-02-05 10:22:50 +0530

    new dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,20 +3,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss',
-    '../../assets/vendor/metisMenu/metisMenu.min.css',
-    '../../assets/vendor/bootstrap/css/bootstrap.min.css',
-    '../../assets/dist/css/sb-admin-2.css',
-    '../../assets/vendor/morrisjs/morris.css',
-    '../../assets/vendor/font-awesome/css/font-awesome.min.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  totalUsedArticle = 0;
-  totalRemaningArticle = 0;
+  totalUsedArticle = 120100;
+  totalRemaningArticle = 10000;
 
-  totalWordProcessed = 0;
-  totalRemainingWords = 0;
+  totalWordProcessed = 25000;
+  totalRemainingWords = 75100;
 
   constructor() { }
 
